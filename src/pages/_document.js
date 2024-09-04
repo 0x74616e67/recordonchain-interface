b70e2967d60bd6288ab18551863857b8c3defee2
0x74616e67 <0x74616e67@gmail.com>
2024-09-05 06:06:16 +0800

    add ICP and copyright

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,6 +7,17 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <footer className="fixed bottom-0 z-1">
+          <a
+            href="https://beian.miit.gov.cn"
+            target="_blank"
+            className="text-blue-500"
+          >
+            陕ICP备2024048059号-1
+          </a>
+          <br></br>
+          <span>© 2024 qukuailianji.com 版权所有</span>
+        </footer>
       </body>
     </Html>
   );
