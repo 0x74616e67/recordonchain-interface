53aa6bf2d6e622909d692bf1169df8665aadce44
0x74616e67 <0x74616e67@gmail.com>
2024-10-10 13:58:44 +0800

    update homepage content

--- src/components/Portrait/index.js
+++ src/components/Portrait/index.js
@@ -1,5 +1,5 @@
 import React, { memo } from "react";
-import Image from "next/image";
+// import Image from "next/image";
 
 const PortraitComponent = memo(function PortraitComponent({
   avatar = "",
@@ -10,7 +10,7 @@ const PortraitComponent = memo(function PortraitComponent({
   return (
     <div className="mb-4 text-base border-[#E2E8F0] border-[1px] p-3 mt-3 rounded">
       <div className="flex">
-        <Image
+        {/* <Image
           src={avatar}
           width={42}
           height={42}
@@ -20,8 +20,8 @@ const PortraitComponent = memo(function PortraitComponent({
             borderRadius: "4px",
           }}
           alt="avatar"
-        ></Image>
-        <div className="flex flex-col ml-2">
+        ></Image> */}
+        <div className="flex flex-col ml-0">
           <span className="font-bold">{name}</span>
           <span className="text-gray0 text-sm">{identity}</span>
         </div>
