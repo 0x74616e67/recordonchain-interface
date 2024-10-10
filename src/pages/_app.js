5677bca95f9d12dd2f143096c888464200e3a55e
0x74616e67 <0x74616e67@gmail.com>
2024-10-10 20:54:49 +0800

    add baidu tongji

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import { IntlProvider } from "use-intl";
 import { useLocaleStore } from "@/utils/store";
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
+import Script from "next/script";
 
 import en from "@/locales/en";
 import zh from "@/locales/zh";
@@ -34,6 +35,19 @@ export default function App({ Component, pageProps }) {
     >
       <Layout>
         <Component {...pageProps} />
+        {process.env.NEXT_PUBLIC_ANALYSIS === "true" ? (
+          <Script id="baiduanalysis">
+            {`
+            var _hmt = _hmt || [];
+            (function() {
+              var hm = document.createElement("script");
+              hm.src = "https://hm.baidu.com/hm.js?e441f0a83da13bd682e8a6546bf0b06f";
+              var s = document.getElementsByTagName("script")[0]; 
+              s.parentNode.insertBefore(hm, s);
+            })();
+          `}
+          </Script>
+        ) : null}
       </Layout>
     </IntlProvider>
   ) : null;
