6771fb77d5665836384df569cda4be38b53480b1
0x74616e67 <0x74616e67@gmail.com>
2024-11-20 21:06:10 +0800

    refactor share panel

--- src/components/SharePanel/index.js
+++ src/components/SharePanel/index.js
@@ -4,7 +4,6 @@ import QRCode from "@/components/QRCode";
 import html2canvas from "html2canvas";
 import Dialog from "@/components/Dialog";
 import { useTranslations } from "use-intl";
-import Button from "../Button";
 
 const imgs = ["/h.jpg", "/v.jpg"];
 
@@ -15,48 +14,73 @@ const SharePanel = React.memo(function SharePanel({
   onOk = () => {},
 }) {
   const t = useTranslations("Common");
-  const [src, setSrc] = useState(imgs[0]);
-
-  const counter = useRef(0);
+  // 为了防止 bg_img 加载完成后 qrcode 还未渲染完，导致 qrcode 缺失，此处优先生成 qrcode，成功后再生成 bg_img
+  const [bgImgSrc, setBgImgSrc] = useState("");
 
   const handleQRCodeSuccess = useCallback(() => {
-    console.log("QRCode success");
+    // 1. qrcode 生成成功后，更新 bg_img 图片
+    setBgImgSrc(imgs[0]);
   }, []);
 
   const handleGalleryClick = useCallback((index) => {
-    setSrc(imgs[index]);
+    setBgImgSrc(imgs[index]);
   }, []);
 
-  const handleDownloadClick = useCallback(() => {
-    if (!counter.current) {
-      html2canvas(document.getElementById("card"), {
-        onclone(dom, element) {
-          element.className = element.className.replace("overflow-auto", "");
-          return element;
-        },
-      }).then(function (canvas) {
-        // 使用toDataURL方法将canvas转换为数据URL
-        var dataURL = canvas.toDataURL("image/png"); // 也可以使用'image/jpeg'
+  // 2. bg_img onLoad 完成后，再生成 share_img
+  // TODO, 这个可能会有点慢，后面可以放一个 loading 的效果
+  const handlebgImgLoad = useCallback((e) => {
+    // 3. 首先需要手动计算 id=bg_img 的 <img> 标签样式，使其填满整个父级元素，且居中显示
+    const parentWidth = document.getElementById("share_img").clientWidth;
+    const width = e.target.naturalWidth;
+    const height = e.target.naturalHeight;
+    const style = {};
 
-        counter.current++;
-        document.body.style.overflow = "hidden";
+    if (width > height) {
+      const targetWidth = parentWidth * (width / height);
 
-        // const imageUrl = document.getElementById("image").src;
-        // 创建一个 <a> 元素
-        const a = document.createElement("a");
-        a.href = dataURL;
-        a.download = "image.jpg"; // 设置文件名
-        a.style.display = "none";
-        // 将 <a> 添加到文档中
-        document.body.appendChild(a);
-        // 触发点击事件
-        a.click();
-        // 移除 <a> 元素
-        document.body.removeChild(a);
+      style.height = `${parentWidth}px`;
+      style.width = `${targetWidth}px`;
+      style.marginLeft = `-${(targetWidth - parentWidth) / 2}px`;
+      style.marginRight = `-${(targetWidth - parentWidth) / 2}px`;
+      style.marginTop = "0px";
+      style.marginBottom = "0px";
+    } else {
+      const targetHeight = parentWidth * (height / width);
 
-        counter.current = 0;
-      });
+      style.width = `${parentWidth}px`;
+      style.height = `${targetHeight}px`;
+      style.marginTop = `-${(targetHeight - parentWidth) / 2}px`;
+      style.marginBottom = `-${(targetHeight - parentWidth) / 2}px`;
+      style.marginLeft = "0px";
+      style.marginRight = "0px";
     }
+
+    html2canvas(document.getElementById("card"), {
+      onclone(dom, element) {
+        element.className = element.className.replace("hidden", "");
+
+        // 更新 id=bg_img 的 <img> 标签样式
+        element.childNodes[0].childNodes[0].style.width = style.width;
+        element.childNodes[0].childNodes[0].style.height = style.height;
+        element.childNodes[0].childNodes[0].style.marginTop = style.marginTop;
+        element.childNodes[0].childNodes[0].style.marginBottom =
+          style.marginBottom;
+        element.childNodes[0].childNodes[0].style.marginLeft = style.marginLeft;
+        element.childNodes[0].childNodes[0].style.marginRight =
+          style.marginRight;
+
+        return element;
+      },
+    }).then(function (canvas) {
+      // 使用toDataURL方法将canvas转换为数据URL
+      var dataURL = canvas.toDataURL("image/png"); // 也可以使用'image/jpeg'
+      const shareImg = document.getElementById("share_img");
+      shareImg.src = dataURL;
+      shareImg.parentElement.scrollTo({
+        top: 0,
+        behavior: "smooth", // 平滑滚动
+      });
+    });
   }, []);
 
   const handleFileUploadChange = useCallback((e) => {
@@ -65,7 +89,7 @@ const SharePanel = React.memo(function SharePanel({
       const reader = new FileReader();
       reader.onload = function (event) {
         const result = event.target.result;
-        document.getElementById("image").src = result;
+        setBgImgSrc(result);
       };
       reader.readAsDataURL(file);
     }
@@ -76,77 +100,60 @@ const SharePanel = React.memo(function SharePanel({
   }
 
   return (
-    <div>
-      <Dialog
-        open={open}
-        onOk={onOk}
-        onCancel={onCancel}
-        okButton={[]}
-        cancelButton={[]}
-        closeable={true}
-      >
-        <div id="card" className="flex flex-col justify-between overflow-auto">
-          <div>
-            <img
-              id="image"
-              src={src}
-              className="object-cover h-[40vh] w-full"
-              alt="share-panel-bg"
-            ></img>
-          </div>
-          <div className="flex-none shrink-0 p-2 border-gray-200 border-[1px] border-t-0">
-            <div className="text-left text-wrap break-words">{tx.message}</div>
-            <div className="text-right mt-4">
-              {formatTimestamp(tx.timestamp)}
-            </div>
-            <QRCode
-              className="mt-4 self-end float-right border-gray-200 border-[1px]"
-              text={getShareURL(tx.chain, tx.hash)}
-              onSuccess={handleQRCodeSuccess}
-              width={100}
-            ></QRCode>
-          </div>
+    <Dialog
+      open={open}
+      onOk={onOk}
+      onCancel={onCancel}
+      okButton={[]}
+      cancelButton={[]}
+      closeable={true}
+    >
+      <div className="flex flex-col justify-between overflow-auto">
+        <img id="share_img" className="" alt="share-img" src="/photo.svg"></img>
+      </div>
+      {/* <div id="card" className="flex flex-col justify-between overflow-auto"> */}
+      <div id="card" className="flex flex-col justify-between hidden z-[-10]">
+        <div className="shrink-0 overflow-hidden">
+          <img
+            id="bg_img"
+            src={bgImgSrc}
+            className="max-w-none max-h-none"
+            alt="share-panel-bg"
+            onLoad={handlebgImgLoad}
+          ></img>
         </div>
-        <div className="mt-4 h-14 flex justify-items-stretch">
-          {imgs.map((i, index) => (
-            <div
-              className={`w-14 h-14 ${index > 0 ? "ml-4" : ""}`}
-              onClick={() => handleGalleryClick(index)}
-              key={i}
-            >
-              <img
-                src={i}
-                className="object-cover w-full h-full"
-                alt="bg"
-              ></img>
-            </div>
-          ))}
-          <div>
-            <input
-              type="file"
-              id="fileInput"
-              accept="image/*"
-              onChange={handleFileUploadChange}
-              className="relative w-14 h-14 ml-4 border-gray-200 border-[1px] focus:outline-none after:content-[''] after:text-red-500 after:absolute after:top-0 after:left-0 after:right-0 after:bottom-0 after:bg-upload after:bg-white after:bg-[length:28px_28px] after:bg-no-repeat after:bg-center !overflow-hidden"
-            ></input>
-          </div>
-          <div className="flex-1 grow">
-            {/* <div
-              className="w-14 h-14 ml-4 flex justify-center items-center bg-blue-600 hover:bg-blue-500 text-white float-right"
-              onClick={handleDownloadClick}
-            >
-              保存
-            </div> */}
-            <Button
-              onClick={handleDownloadClick}
-              className="!w-14 float-right mt-5"
-            >
-              {t("button.save")}
-            </Button>
-          </div>
+        <div className="flex-none shrink-0 p-2 border-gray-200 border-[1px] border-t-0">
+          <div className="text-left text-wrap break-words">{tx.message}</div>
+          <div className="text-right mt-4">{formatTimestamp(tx.timestamp)}</div>
+          <QRCode
+            className="mt-4 self-end float-right border-gray-200 border-[1px]"
+            text={getShareURL(tx.chain, tx.hash)}
+            onSuccess={handleQRCodeSuccess}
+            width={100}
+          ></QRCode>
         </div>
-      </Dialog>
-    </div>
+      </div>
+      <div className="mt-4 h-14 flex justify-items-stretch">
+        {imgs.map((i, index) => (
+          <div
+            className={`w-14 h-14 ${index > 0 ? "ml-4" : ""}`}
+            onClick={() => handleGalleryClick(index)}
+            key={i}
+          >
+            <img src={i} className="object-cover w-full h-full" alt="bg"></img>
+          </div>
+        ))}
+        <div>
+          <input
+            type="file"
+            id="fileInput"
+            accept="image/*"
+            onChange={handleFileUploadChange}
+            className="relative w-14 h-14 ml-4 border-gray-200 border-[1px] focus:outline-none after:content-[''] after:text-red-500 after:absolute after:top-0 after:left-0 after:right-0 after:bottom-0 after:bg-upload after:bg-white after:bg-[length:28px_28px] after:bg-no-repeat after:bg-center !overflow-hidden"
+          ></input>
+        </div>
+      </div>
+    </Dialog>
   );
 });
 
