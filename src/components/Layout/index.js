a1057dd4fff1e19948994b7cbd09269b9951abc7
0x74616e67 <0x74616e67@gmail.com>
2024-12-19 21:42:37 +0800

    add new logo

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -7,8 +7,9 @@ function Layout({ children }) {
 
   return (
     <>
-      <div className="flex h-16 items-center justify-between border-b px-4 text-2xl">
-        <h1>{t("title")}</h1>
+      <div className="flex h-16 items-center border-b px-3 text-2xl">
+        <img className="mr-2 h-10 w-10 border" src="/logo.svg"></img>
+        <h1 className="text-blue-600">{t("title")}</h1>
       </div>
       <main className="relative h-[calc(100%-128px)] overflow-auto p-4">
         {children}
